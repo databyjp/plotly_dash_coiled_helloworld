37ffbbd74adc62b8d12fcfac77dac4b4cb6576e6
JP Hwang <me@jphwang.com>
2021-07-27 23:47:53 +0800

    aborting a layout - not interesting

--- app.py
+++ app.py
@@ -36,48 +36,32 @@ df = df[df["trip_distance"] > 0]
 df = df[(df["fare_amount"] > 0) & (df["fare_amount"] < 10000)]
 ignore_IDs = [264, 265]  # Unknown locations
 df = df[-(df["PULocationID"].isin(ignore_IDs) | df["DOLocationID"].isin(ignore_IDs))]
+df = df[df["tpep_pickup_datetime"] < df["tpep_dropoff_datetime"]]
 
 # Add new columns
-df = df.assign(hour=pd.to_datetime(df["tpep_pickup_datetime"]).apply(lambda x: x.hour))
-df = df.assign(wkend=pd.to_datetime(df["tpep_pickup_datetime"]).apply(lambda x: x.weekday() >= 5))
+df = df.assign(hour=pd.to_datetime(df["tpep_pickup_datetime"]).dt.hour)
+df = df.assign(day=pd.to_datetime(df["tpep_pickup_datetime"]).dt.weekday)
+df = df.assign(wkend=df["day"] >= 5)
+df = df.assign(triptime=pd.to_datetime(df["tpep_dropoff_datetime"])-pd.to_datetime(df["tpep_pickup_datetime"]))
+df = df.assign(avg_spd=df["trip_distance"] / df.triptime.dt.seconds * 3600)
+
+df = df[df["avg_spd"] <= 50]  # NY taxis are fast but not *that* fast
 
 # Group data by desired parameters
 avg_fare_per_dist = df["fare_amount"].mean() / df["trip_distance"].mean()
-grp_df = df.groupby(["wkend", "hour"]).agg({'tip_amount': 'mean', 'fare_amount': 'mean', 'trip_distance': 'mean', 'passenger_count': 'mean', 'VendorID': 'count'})
+grp_df = df.groupby(["wkend", "hour"]).agg({'tip_amount': 'mean', 'fare_amount': 'mean', 'trip_distance': 'mean', 'passenger_count': 'mean', 'VendorID': 'count', 'avg_spd': 'mean'})
 grp_df.reset_index(inplace=True)
 grp_df = grp_df.assign(fare_per_dist=grp_df["fare_amount"]/grp_df["trip_distance"]-avg_fare_per_dist)
 grp_df = grp_df.assign(tip_per_fare=grp_df["tip_amount"]/grp_df["fare_amount"])
+grp_df = grp_df.assign(tip_per_dist=grp_df["tip_amount"]/grp_df["trip_distance"])
 grp_df = grp_df.rename({'VendorID': 'count'}, axis=1)
 
-bar_fig_a = px.bar(grp_df, x="hour", y="count", barmode="stack", color="wkend", height=300)
-bar_fig_b = px.bar(grp_df, x="hour", y="fare_per_dist", color="wkend", facet_col="wkend", height=300)
+bar_fig = px.bar(grp_df, x="hour", y="count", barmode="stack", color="wkend", height=200)
+bar_fig.update_layout(margin=dict(l=5, r=5, t=5, b=5))
 
-scatter_fig = px.scatter(grp_df, x="tip_per_fare", y="fare_per_dist", size="count", color="hour",
-                         color_continuous_scale=px.colors.cyclical.IceFire, facet_col="wkend", height=300)
-
-locs_count = df.groupby(["PULocationID"]).count()['VendorID']
-locs_count = locs_count[locs_count > 100000]
-locs = locs_count.index
-
-loc_df = df.groupby(["wkend", "hour", "PULocationID"]).agg({'tip_amount': 'mean', 'fare_amount': 'mean', 'trip_distance': 'mean', 'passenger_count': 'mean', 'VendorID': 'count'})
-loc_df.reset_index(inplace=True)
-loc_df = loc_df[loc_df["PULocationID"].isin(locs)]
-loc_df = loc_df.assign(fare_per_dist=loc_df["fare_amount"]/loc_df["trip_distance"]-avg_fare_per_dist)
-loc_df = loc_df.rename({'VendorID': 'count'}, axis=1)
-
-loc_scatter_df = px.scatter(loc_df, x="hour", y="fare_per_dist", size="count", color="PULocationID",
-                            color_continuous_scale=px.colors.cyclical.IceFire, facet_col="wkend", height=300)
-
-stdevs = list()
-for loc in locs:
-    tmp_df = loc_df[loc_df["PULocationID"] == loc]
-    stdevs.append(tmp_df["fare_per_dist"].std())
-
-stdev_df = pd.DataFrame([locs, stdevs]).transpose()
-stdev_df.columns = ["PULocationID", "stdev"]
-stdev_df["PULocationID"] = stdev_df["PULocationID"].astype(int)
-stdev_df.sort_values("stdev", ascending=False, inplace=True)
-
+scatter_fig = px.scatter(grp_df, x="trip_distance", y="fare_amount", size="count", color="hour",
+                           color_continuous_scale=px.colors.cyclical.IceFire, facet_col="wkend", height=300)
+scatter_fig.update_layout(margin=dict(l=5, r=5, t=20, b=5))
 
 # DASH LAYOUT
 
@@ -92,24 +76,34 @@ body = html.Div(
         dbc.Row([
             dbc.Col([
                 dbc.Card([
-                    dbc.CardHeader(html.H5("This is a test app")),
+                    dbc.CardHeader(html.H6("This is a test app")),
                     dbc.CardBody('This app shows how Dash apps can be quickly adapted to use Coiled as its back end'),
                 ]),
             ], sm=12),
         ]),
         dbc.Row([
             dbc.Col([
-                dcc.Graph(figure=bar_fig_a, id="bar-fig-a")
-            ], sm=12, md=6),
+                dcc.Graph(figure=bar_fig, id="bar-fig-a")
+            ], sm=12, md=8),
             dbc.Col([
-                dcc.Graph(figure=bar_fig_b, id="bar-fig-b")
-            ], sm=12, md=6),
-        ]),
+                dbc.Card([
+                    dbc.CardHeader(html.H6("Volume ")),
+                    dbc.CardBody("Although morning traffic ramps up quickly from the lull of 4-5am, the peak number of pick-ups occur at 6-7pm.")
+                ]),
+            ], sm=12, md=4),
+        ], className="mt-2"),
         dbc.Row([
             dbc.Col([
-                dcc.Graph(figure=scatter_fig)
-            ], sm=12),
-        ]),
+                dbc.Card([
+                    dbc.CardHeader(html.H6("How does typical travel distance and cost change across a day?")),
+                    dbc.CardBody("The average fare increases almostly linearly with average trip distance, except at certain hours. A closer inspection indicates that the fare is higher during peak hours, indicating that this is caused by traffic and slow travel time.")
+                ]),
+            ], sm=12, md=4),
+            dbc.Col([
+                dcc.Graph(figure=scatter_fig, id="bar-fig-b")
+            ], sm=12, md=8),
+        ], className="mt-2"),
+        dbc.Row(html.H3("Find similar neighborhoods - by your own metrics".upper()), className="my-2"),
         dbc.Row([
             dbc.Col([
                 dbc.Card([
@@ -120,9 +114,20 @@ body = html.Div(
                         step=1,
                         value=[9, 17]
                     ),
+                    dcc.Dropdown(
+                        id='prop-select'
+                    )
                 ]),
-            ], sm=12, md=6),
-        ])
+            ], sm=12, md=4),
+            dbc.Col([
+                dbc.Card([
+                    dbc.CardHeader(html.H6("Clustering results")),
+                    dbc.CardBody(
+                        dcc.Graph(figure=None, id="cluster-output")
+                    )
+                ])
+            ])
+        ], className="mt-2")
     ])
 )
 
